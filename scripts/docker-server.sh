a41427a8b5d87d9b928840450335c14b0f36fca1
Pierrick Couderc <pierrick.couderc@ocamlpro.com>
2018-05-11 11:58:32 +0200

    Removes docker's standard output

--- scripts/docker-server.sh
+++ scripts/docker-server.sh
@@ -7,19 +7,24 @@ container_name="learn-ocaml-docker-container"
 function print_usage() {
     printf "Usage: %s COMMAND <OPTIONS>\n\
 Commands:\n\
-  init  \t Initializes the server and run it.\n\
+  init  \t Initializes the server and runs it.\n\
   start \t Starts an already initialized server.\n\
   restart\t Restarts the current server (starts it if it is not running).\n\
   stop  \t Stops the server.\n\
-  remove\t Remove the current server from the existing containers.\n\
-  force-remove\t Stops and remove de container.\n\
-  \n\
+  remove\t Stops the server and removes it from the existing containers.\n\
 Options:\n\
   -port (default = 9090): Port used to communicate with the server.\n\
   -docker-image (default = learn-ocaml-docker): name of the generated\
   docker image.\n\
   -container-name (default = learn-ocaml-docker-container): name of the\
   container in docker container system.\n" "$0"
+}
+
+function run_image () { # port, container, image
+    docker run -d \
+           -p "$1":9090 \
+           --name "$2" "$3"
+    
 }
 
 while [[ $# -gt 0 ]]; do
@@ -56,25 +61,20 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 case $1 in
     init)
-    docker run -d \
-           -p "$port":9090 \
-           --name "$container_name" "$docker_image"
+    run_image "$port" "$container_name" "$docker_image" 1> /dev/null;
     ;;
     start)
-    docker container start "$container_name"
+    docker container start "$container_name" 1> /dev/null
     ;;
     restart)
-    docker container restart "$container_name"
+    docker container restart "$container_name" 1> /dev/null
     ;;
     stop)
-    docker container stop "$container_name"
+    docker container stop "$container_name" 1> /dev/null
     ;;    
     remove)
-    docker container rm "$container_name"
-    ;;
-    force-remove)
-    docker container stop "$container_name" &&
-    docker container rm "$container_name"
+    docker container stop "$container_name" 1> /dev/null;
+    docker container rm "$container_name" 1> /dev/null
     ;;
     *)
     echo "Unknown command $1"
