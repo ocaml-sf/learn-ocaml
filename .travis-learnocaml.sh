52679c90f44e8f3a38e915f0375abea1794c3a96
Yann Regis-Gianas <yrg@irif.fr>
2018-06-13 19:59:26 +0200

    Fix travis script to convert opam repository.

--- .travis-learnocaml.sh
+++ .travis-learnocaml.sh
@@ -5,7 +5,7 @@
 opam install -y opam-devel
 sudo cp /home/travis/.opam/4.05.0/lib/opam-devel/* /usr/local/bin
 hash -r
-opam init -a
+opam upgrade -y
 opam install . -y --deps
 make PROCESSING_JOBS=1
 make opaminstall
