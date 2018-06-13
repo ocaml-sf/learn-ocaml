a5034af7ab19049880421f8a088bef91cc321154
Yann Regis-Gianas <yrg@irif.fr>
2018-06-13 15:11:57 +0200

    Fix travis script to activate opam 2.0.0 after its installation.

--- .travis-learnocaml.sh
+++ .travis-learnocaml.sh
@@ -3,6 +3,8 @@
 
 . .travis-ocaml.sh
 opam install -y opam-devel
+sudo cp /home/travis/.opam/4.05.0/lib/opam-devel/* /usr/local/bin
+hash -r
 opam install . -y --deps
 make PROCESSING_JOBS=1
 make opaminstall
