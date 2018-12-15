5ffe0b827e8dc0f1d4b8d9abf5afb47aef025da2
Yann Regis-Gianas <yrg@irif.fr>
2018-12-15 19:29:10 +0100

    Replace source by /bin/sh.

--- .ci-macosx.sh
+++ .ci-macosx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 curl -LO https://raw.githubusercontent.com/GiovanniBussi/macports-ci/master/macports-ci
-sudo source ./macports-ci install
+sudo /bin/sh ./macports-ci install
 sudo port install opam
 opam --version
