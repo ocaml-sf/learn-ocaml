fa49b9ca0b0dabb734caf287ba304ea98d3bc2ca
Yann Regis-Gianas <yrg@irif.fr>
2019-01-29 13:19:54 +0100

    Update copyright.

--- src/main/learnocaml_server_args.ml
+++ src/main/learnocaml_server_args.ml
@@ -1,6 +1,6 @@
 (* This file is part of Learn-OCaml.
  *
- * Copyright (C) 2018 OCamlPro.
+ * Copyright (C) 2019 OCaml Software Foundation.
  *
  * Learn-OCaml is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
