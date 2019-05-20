9ad957a217e820ac8d3d5b825711428c94703b54
Alexandre Moine <alexandre@moine.me>
2019-05-20 17:47:41 +0200

    can test multiple corpus

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -8,58 +8,71 @@ red () {
     echo -e "\e[31mNOT OK: $1\e[0m"
 }
 
+run_server (){
+    # Build the reporistory
+    pushd $TMP > /dev/null
+    learn-ocaml build --repo test-repo
+    if [ $? -ne 0 ]; then
+	echo Build failed
+	exit 1
+    fi
+
+    # Run the server in background
+    learn-ocaml serve > /dev/null &
+    popd > /dev/null
+
+    # Wait for the server to be initialized
+    sleep 2
+}
+
 # Temporary directory
 TMP=$(mktemp -d)
 
-cp -r ../demo-repository $TMP/test-repo
-
-# Build the reporistory
-pushd $TMP
-learn-ocaml build --repo test-repo
-if [ $? -ne 0 ]; then
-    echo Build failed
-    exit 1
-fi
-
-# Run the server in background
-learn-ocaml serve > /dev/null &
-popd
-
-# Wait for the server to be initialized
-sleep 2
-
-# Get the token
-TOKEN=$(find $TMP/sync -name \*.json -printf '%P' | sed 's|/|-|g' | sed 's|-save.json||')
-
 # For each subdirectory
-for DIR in `find . -type d ! -path .`
+for DIR in `ls -d */`
 do
     pushd $DIR > /dev/null
-    for TOSEND in `find . -name "*.ml" -type f -printf "%f\n"`
+
+    echo " :*: Doing $DIR"
+
+    cp -r -L repo $TMP/test-repo
+
+    run_server
+
+    # Get the token
+    TOKEN=$(find $TMP/sync -name \*.json -printf '%P' | sed 's|/|-|g' | sed 's|-save.json||')
+    
+    for SUBDIR in `find .  -maxdepth 1 -type d ! -path . ! -path ./repo`
     do
-	# Grade file
-	learn-ocaml-client --server http://localhost:8080 --token "$TOKEN" $TOSEND > res.txt
-	if [ $? -ne 0 ]
-	then
-	   red "$DIR/$TOSEND"
-	fi
-	# If there is something to compare
-	if [ -f "$TOSEND.txt" ]
-	then
-	    diff res.txt "$TOSEND.txt"
+	pushd $SUBDIR > /dev/null
+
+	for TOSEND in `find . -name "*.ml" -type f -printf "%f\n"`
+	do
+	    # Grade file
+	    learn-ocaml-client --server http://localhost:8080 --token "$TOKEN" --id="$SUBDIR" $TOSEND > res.txt
 	    if [ $? -ne 0 ]
 	    then
-	       red "$DIR/$TOSEND"
-	       break 2
+		red "$DIR$TOSEND"
 	    fi
-	fi
-        green "$DIR/$TOSEND"
-	rm res.txt
+	    # If there is something to compare
+	    if [ -f "$TOSEND.txt" ]
+	    then
+		diff res.txt "$TOSEND.txt"
+		if [ $? -ne 0 ]
+		then
+		    red "$DIR$TOSEND"
+		    break
+		fi
+	    fi
+            green "$DIR$TOSEND"
+	    rm res.txt
+	done
+
+	popd > /dev/null
+	# Cleanup
+	rm -rf $TMP/*
+
+	kill $!
     done
     popd > /dev/null
 done
-
-# Cleanup
-rm -rf $TMP
-
-kill $!
