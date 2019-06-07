745f503f7e791e79e15538bd91064890aa9fe7e1
Alexandre Moine <alexandre@moine.me>
2019-06-07 09:07:39 +0200

    switch to full rec

--- mesocaml/ast_utils.ml
+++ mesocaml/ast_utils.ml
@@ -1,22 +1,27 @@
 open Parsetree
-open Ast_iterator
 open Asttypes
 
 (* TODO hash node *)
 
-let hash_string_lst accr x xs=
-  accr :=
-    Digest.string @@
-      String.concat "" (Digest.string x::xs)
+let hash_string_lst x xs=
+  Digest.string @@
+    String.concat "" (Digest.string x::xs)
 
-let hash_lst accr f self x xs =
+(* let hash_lst accr f self x xs =
   let res =
     List.fold_left
       (fun acc x -> f self x; !accr :: acc)
       [] xs in
-  hash_string_lst accr x (List.sort compare res)
+  hash_string_lst accr x (List.sort compare res) *)
 
-let hash_lst_anon accr f self xs = hash_lst accr f self "" xs
+let hash_lst f x xs =
+  let res =
+    List.fold_left
+      (fun acc x -> f x :: acc)
+      [] xs in
+  hash_string_lst x (List.sort compare res)
+
+let hash_lst_anon f xs = hash_lst f "" xs
 
 let hash_option f =
   function
@@ -29,152 +34,89 @@ let hash_rec_flag = function
 
 let hash_arg_label _ = Digest.string "arg_label" (* Important ? *)
 
-let hash_iterator accr =
-  let gaccr f x = f x; !accr in
-  let attribute self (_,payload) =
-    hash_string_lst accr
-      "attribute"
-      [gaccr (self.payload self) payload]
-    in
-  let attributes self =
-    hash_lst accr self.attribute self "attributes"
-  in
-  let case self pc =
-    hash_string_lst accr "case"
-      [ gaccr (self.pat self) pc.pc_lhs
-      ; hash_option (gaccr (self.expr self)) pc.pc_guard
-      ; gaccr (self.expr self) pc.pc_rhs
-      ]
-  in
-  let cases self =
-    hash_lst accr self.case self "cases"
-  in
-  let hash_expr_desc self = function
-    | Pexp_ident    _ -> accr := Digest.string "ident"
-    | Pexp_constant _ -> accr := Digest.string "const"
-    | Pexp_let (r,valbindl,e) ->
-       hash_string_lst accr "let"
-         [ hash_rec_flag r
-         ; gaccr (hash_lst_anon accr self.value_binding self) valbindl
-         ; gaccr (self.expr self) e
-         ]
-    | Pexp_function xs ->
-       hash_string_lst accr
-         "function"
-         [gaccr (self.cases self) xs]
-    | Pexp_fun (arg,oe,p,e) ->
-       hash_string_lst accr "fun"
-         [ hash_arg_label arg
-         ; hash_option (gaccr (self.expr self)) oe
-         ; gaccr (self.pat self) p
-         ; gaccr (self.expr self) e
-         ]
-    | Pexp_apply (e,xs) ->
-       hash_string_lst accr "apply" @@
-           gaccr (self.expr self) e ::
-             (List.map  (fun (_,x) -> gaccr (self.expr self) x) xs)
-    | Pexp_match (e,cl) ->
-       hash_string_lst accr "match"
-         [ gaccr (self.expr self) e
-         ; gaccr (self.cases self) cl
-         ]
-    | Pexp_try (e,cl) ->
-       hash_string_lst accr "try"
-         [ gaccr (self.expr self) e
-         ; gaccr (self.cases self) cl
-         ]
-    | Pexp_tuple xs ->
-       hash_lst accr self.expr self "tuple" xs
-    | Pexp_ifthenelse (i,f,e) ->
-       hash_string_lst accr "ifthenelse"
-         [ gaccr (self.expr self) i
-         ; gaccr (self.expr self) f
-         ; hash_option (gaccr (self.expr self)) e
-         ] 
-    | _ ->
-       accr := Digest.string "";
-       raise Exit
-  in
-  let expr self e =
-    try
-    hash_string_lst accr "expr"
-      [ gaccr (hash_expr_desc self) e.pexp_desc;
-        gaccr (self.attributes self) e.pexp_attributes
-      ]
-    with Exit -> default_iterator.expr self e
-  in
-  let hash_pattern_desc self = function
-    | Ppat_any -> accr := Digest.string "any"
-    | Ppat_var _ -> accr := Digest.string "var"
-    | Ppat_constant _ -> accr := Digest.string "constant"
-    | Ppat_interval _ -> accr := Digest.string "interval"
-    | Ppat_alias (p,_) ->
-       hash_string_lst accr "alias"
-         [ gaccr (self.pat self) p ]
-    | Ppat_array xs ->
-       hash_string_lst accr "array" @@
-         (List.map  (gaccr (self.pat self)) xs)
-    | Ppat_or (u,v) ->
-       hash_string_lst accr "or"
-         [ gaccr (self.pat self) u
-         ; gaccr (self.pat self) v]
-    | _ -> accr := Digest.string ""
-  in
-  let pat self p =
-    hash_string_lst accr "pat"
-      [ gaccr (hash_pattern_desc self) p.ppat_desc
-      ; gaccr (self.attributes self) p.ppat_attributes
-      ]
-  in
-  let structure self =
-    hash_lst accr self.structure_item self "structure" in
-  let structure_item self x =
-      match x.pstr_desc with
-      | Pstr_eval (e,a) ->
-         hash_string_lst accr "eval"
-           [ gaccr (self.expr self) e
-           ; gaccr (self.attributes self) a
-           ]
-      | Pstr_value (r,valbindl) ->
-         hash_string_lst accr "value"
-           [ hash_rec_flag r
-           ; gaccr (hash_lst_anon accr self.value_binding self) valbindl
-           ]
-      | Pstr_attribute a -> self.attribute self a
-      | Pstr_exception _ -> accr := Digest.string "exception" 
-      | _ ->
-         accr := "";
-         default_iterator.structure_item self x
-  in
-  let value_binding self e =
-    hash_string_lst accr "value_binding"
-      [ gaccr (self.pat self) e.pvb_pat;
-        gaccr (self.expr self) e.pvb_expr;
-        gaccr (self.attributes self) e.pvb_attributes;
-      ]
-  in
-  { default_iterator with
-      attribute
-    ; attributes
-    ; case
-    ; cases
-    ; expr
-    ; extension = attribute
-    ; pat
-    (* ; payload *)
-    ; structure
-    ; structure_item
-    ; value_binding
-  }
-
-let hash_of_struct structure =
-  let accr = ref "" in
-  let hi = hash_iterator accr in
-  hi.structure hi structure;
-  !accr
+(* TODO attributes *)
+let rec hash_value_binding x =
+  hash_string_lst "value_binding"
+    [ hash_pattern x.pvb_pat
+    ; hash_expression x.pvb_expr
+    ]
+and hash_pattern_desc = function
+  | Ppat_any -> Digest.string "any"
+  | Ppat_var _ -> Digest.string "var"
+  | Ppat_constant _ -> Digest.string "constant"
+  | Ppat_interval _ -> Digest.string "interval"
+  | Ppat_alias (p,_) ->
+     hash_string_lst "alias"
+       [ hash_pattern p ]
+  | Ppat_array xs ->
+     hash_string_lst "array" @@
+       (List.map  hash_pattern xs)
+  | Ppat_or (u,v) ->
+     hash_string_lst "or"
+       [ hash_pattern u
+       ; hash_pattern v]
+  | _ -> failwith "hash_pattern_desc"
+and hash_pattern x = hash_pattern_desc x.ppat_desc
+and hash_expression x = hash_expression_desc x.pexp_desc
+and hash_expression_desc = function
+  | Pexp_ident    _ -> Digest.string "ident"
+  | Pexp_constant _ -> Digest.string "const"
+  | Pexp_let (r,valbindl,e) ->
+     hash_string_lst "let"
+       [ hash_rec_flag r
+       ; hash_lst_anon hash_value_binding valbindl
+       ; hash_expression e
+       ]
+  | Pexp_function xs ->
+     hash_string_lst
+       "function"
+       [hash_cases xs]
+  | Pexp_fun (arg,oe,p,e) ->
+     hash_string_lst "fun"
+       [ hash_arg_label arg
+       ; hash_option (hash_expression) oe
+       ; hash_pattern p
+       ; hash_expression e
+       ]
+  | Pexp_apply (e,xs) ->
+     hash_string_lst "apply" @@
+       hash_expression e ::
+         (List.map  (fun (_,x) -> hash_expression x) xs)
+  | Pexp_match (e,cl) ->
+     hash_string_lst "match"
+       [ hash_expression e
+       ; hash_cases cl
+       ]
+  | Pexp_try (e,cl) ->
+     hash_string_lst "try"
+       [ hash_expression e
+       ; hash_cases cl
+       ]
+  | Pexp_tuple xs ->
+     hash_lst hash_expression "tuple" xs
+  | Pexp_ifthenelse (i,f,e) ->
+     hash_string_lst "ifthenelse"
+       [ hash_expression i
+       ; hash_expression f
+       ; hash_option (hash_expression) e
+       ]
+  | _ -> failwith "hash_expression_desc"
+and hash_cases xs = hash_lst hash_case "cases" xs
+and hash_case pc =
+  hash_string_lst "case"
+    [ hash_pattern pc.pc_lhs
+    ; hash_option hash_expression pc.pc_guard
+    ; hash_expression pc.pc_rhs
+    ]
+(* and hash_structure xs = hash_lst hash_structure_item "structure" xs *)
+and hash_structure_item x =
+  match x.pstr_desc with
+  | Pstr_value (p,r) ->
+     hash_string_lst "value" @@
+       [ hash_rec_flag p
+       ; hash_lst hash_value_binding "value_binding" r
+       ]
+  | _ -> failwith "hash_structure_item"
 
 let hash_of_bindings (r,v) =
-  let accr = ref "" in
-  let hi = hash_iterator accr in
-  hi.structure_item hi {pstr_desc=(Pstr_value (r,v)); pstr_loc=Location.none};
-  !accr
+  hash_structure_item {pstr_desc=(Pstr_value (r,v)); pstr_loc=Location.none}
--- mesocaml/ast_utils.mli
+++ mesocaml/ast_utils.mli
@@ -1,4 +1,3 @@
 open Parsetree
 
-val hash_of_struct : structure -> string
 val hash_of_bindings : (Asttypes.rec_flag*value_binding list) -> string
