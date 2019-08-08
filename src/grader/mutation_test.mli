448bf4e2cb8c85e03a8fe96972bf4042529629e2
Aliya Hameer <adhameer@gmail.com>
2019-08-08 17:47:05 -0400

    Mutation testing: add option to run tests on own implementation

--- src/grader/mutation_test.mli
+++ src/grader/mutation_test.mli
@@ -26,6 +26,7 @@ val run_test_against_mutant: ('a -> 'b) -> ('a * 'b) -> bool
 (** Running mutation tests on a student's test suite.
     For testing a function called [foo], the student's tests should
     be in a variable called [foo_tests].
+
     This module needs to be instantiated with an instance of
     Test_lib, which is available to the grader code:
 
@@ -45,26 +46,44 @@ val run_test_against_mutant: ('a -> 'b) -> ('a * 'b) -> bool
 
     The optional argument [~points] specifies how many points
     should be given for each mutant exposed by the test suite.
+    If [test_student_soln] is [true] (as it is by default),
+    also runs the student's test suite against the student's own
+    implementation and reports the results.
 *)
 module type S = sig
   val test_unit_tests_1:
     ?points: int ->
+    ?test_student_soln: bool ->
     ('a -> 'b) Ty.ty -> string -> ('a -> 'b) mutant list -> Learnocaml_report.t
   val test_unit_tests_2:
     ?points: int ->
+    ?test_student_soln: bool ->
     ('a -> 'b -> 'c) Ty.ty -> string -> ('a -> 'b -> 'c) mutant list -> Learnocaml_report.t
   val test_unit_tests_3:
     ?points: int ->
+    ?test_student_soln: bool ->
     ('a -> 'b -> 'c -> 'd) Ty.ty
     -> string
     -> ('a -> 'b -> 'c -> 'd) mutant list
     -> Learnocaml_report.t
   val test_unit_tests_4:
     ?points: int ->
+    ?test_student_soln: bool ->
     ('a -> 'b -> 'c -> 'd -> 'e) Ty.ty
     -> string
     -> ('a -> 'b -> 'c -> 'd -> 'e) mutant list
     -> Learnocaml_report.t
+
+  (* To be called on a report returned by one of the above 4 functions,
+     for checking whether the student passed or failed mutation testing.
+     The [Learnocaml_report.result] function is not sufficient for
+     checking this since a report will register as a failure if the
+     student's implementation does not pass all of their own tests, even
+     if the student did pass mutation testing.
+     If this function is called on a report that did not result from
+     one of the above 4 functions, the result is undefined.
+  *)
+  val passed_mutation_testing: Learnocaml_report.t -> bool
 end
 
 module Make (Test_lib: Test_lib.S) : S
