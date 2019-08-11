807b6023ecb4689c432d51686a4023a6f72b8494
Aliya Hameer <adhameer@gmail.com>
2019-08-11 16:23:12 -0400

    Mutation testing: allow custom output comparison

--- src/grader/mutation_test.mli
+++ src/grader/mutation_test.mli
@@ -11,24 +11,38 @@ type 'a test_result =
   | Fail of 'a
   | Err of exn
 
+(** A mutant is a pair of a string describing the mutant and the
+    mutant function itself.
+*)
 type 'a mutant = string * 'a
 
-(** Run a test (a pair of input and expected output) on a function. *)
-val run_test_against: ('a -> 'b) -> ('a * 'b) -> 'b test_result
+(** Run a test (a pair of input and expected output) on a function.
+    The [compare] parameter specifies a comparison function for
+    comparing the expected and actual outputs, and defaults to
+    structural equality ([(=)]).
+*)
+val run_test_against:
+  ?compare: ('b -> 'b -> bool) ->
+  ('a -> 'b) -> ('a * 'b) -> 'b test_result
 
 (** Run a test (a pair of input and expected output) on a mutant.
     Returns true if the mutant *fails* the test, either by deviating
     from the expected output or by raising an error.
     Returns false if the mutant *passes* the test.
+    The [compare] parameter specifies a comparison function for
+    comparing the expected and actual outputs, and defaults to
+    structural equality ([(=)]).
 *)
-val run_test_against_mutant: ('a -> 'b) -> ('a * 'b) -> bool
+val run_test_against_mutant:
+  ?compare: ('b -> 'b -> bool) ->
+  ('a -> 'b) -> ('a * 'b) -> bool
 
 (** Running mutation tests on a student's test suite.
-    For testing a function called [foo], the student's tests should
+    For testing a function call [foo], the student's tests should
     be in a variable called [foo_tests].
 
     This module needs to be instantiated with an instance of
-    Test_lib, which is available to the grader code:
+    [Test_lib], which is available to the grader code:
 
     {[
       module M = Mutation_test.Make (Test_lib)
@@ -49,19 +63,25 @@ val run_test_against_mutant: ('a -> 'b) -> ('a * 'b) -> bool
     If [test_student_soln] is [true] (as it is by default),
     also runs the student's test suite against the student's own
     implementation and reports the results.
+    The [test] parameter specifies a comparison function for the
+    expected and actual outputs, and defaults to structural
+    equality ([(=)]).
 *)
 module type S = sig
   val test_unit_tests_1:
     ?points: int ->
     ?test_student_soln: bool ->
+    ?test: ('b -> 'b -> bool) ->
     ('a -> 'b) Ty.ty -> string -> ('a -> 'b) mutant list -> Learnocaml_report.t
   val test_unit_tests_2:
     ?points: int ->
     ?test_student_soln: bool ->
+    ?test: ('c -> 'c -> bool) ->
     ('a -> 'b -> 'c) Ty.ty -> string -> ('a -> 'b -> 'c) mutant list -> Learnocaml_report.t
   val test_unit_tests_3:
     ?points: int ->
     ?test_student_soln: bool ->
+    ?test: ('d -> 'd -> bool) ->
     ('a -> 'b -> 'c -> 'd) Ty.ty
     -> string
     -> ('a -> 'b -> 'c -> 'd) mutant list
@@ -69,6 +89,7 @@ module type S = sig
   val test_unit_tests_4:
     ?points: int ->
     ?test_student_soln: bool ->
+    ?test: ('e -> 'e -> bool) ->
     ('a -> 'b -> 'c -> 'd -> 'e) Ty.ty
     -> string
     -> ('a -> 'b -> 'c -> 'd -> 'e) mutant list
