6b9fef3cef1341a0ae59d8425417d098d888c5bd
Alban Gruin <alban.gruin@univ-tlse3.fr>
2020-07-07 16:38:23 +0200

    launch.html: does not request credentials if a user is already authenticated
    Signed-off-by: Alban Gruin <alban.gruin@univ-tlse3.fr>

--- src/app/learnocaml_lti_main.ml
+++ src/app/learnocaml_lti_main.ml
@@ -16,6 +16,10 @@ let login_returning_id, login_returning = id "login-returning"
 let button_yes_id, button_yes = id "first-connection-yes"
 let button_no_id, button_no = id "first-connection-no"
 
+let login_csrf_input_id, login_csrf_input = id "login-csrf-input"
+let login_id_input_id, login_id_input = id "login-id-input"
+let login_hmac_input_id, login_hmac_input = id "login-hmac-input"
+
 let set_string_translations =
   List.iter
     (fun (id, text) ->
@@ -32,15 +36,39 @@ let init_dialogs () =
       Dom_html.window##.location##assign (Js.string "/");
       true)
 
+let try_stored_token () =
+  try
+    let token = Learnocaml_local_storage.(retrieve sync_token) in
+    let parameters =
+      [("token", [Learnocaml_data.Token.to_string token]);
+       ("csrf", [Js.to_string (Tyxml_js.To_dom.of_input login_csrf_input)##.value]);
+       ("user-id", [Js.to_string (Tyxml_js.To_dom.of_input login_id_input)##.value]);
+       ("hmac", [Js.to_string (Tyxml_js.To_dom.of_input login_hmac_input)##.value])]
+      |> Uri.encoded_of_query |> Js.string |> Js.some in
+    let request = Js_of_ocaml.XmlHttpRequest.create () in
+    request##(_open (Js.string "POST") (Js.string "/launch/login") (Js._false));
+    request##(setRequestHeader (Js.string "Content-type")
+                (Js.string "application/x-www-form-urlencoded"));
+    request##(send parameters);
+    if request##.status = 200 then
+      Ok ()
+    else
+      Error ()
+  with Not_found ->
+    Error ()
+
 let () =
   (match Js_utils.get_lang () with Some l -> Ocplib_i18n.set_lang l | None -> ());
-  init_dialogs ();
-  set_string_translations [
-      "txt_first_connection_dialog", [%i"First connection"];
-      "txt_first_connection_question", [%i"Do you have a Learn OCaml account?"];
-      "txt_button_yes", [%i"Yes"];
-      "txt_button_no", [%i"No"];
-      "txt_returning_token", [%i"Enter your token"];
-      "txt_returning_token_label", [%i"Token"];
-      "txt_button_connect", [%i"Connect"]
-    ]
+  match try_stored_token () with
+  | Ok () -> Dom_html.window##.location##assign (Js.string "/")
+  | Error () ->
+     init_dialogs ();
+     set_string_translations [
+         "txt_first_connection_dialog", [%i"First connection"];
+         "txt_first_connection_question", [%i"Do you have a Learn OCaml account?"];
+         "txt_button_yes", [%i"Yes"];
+         "txt_button_no", [%i"No"];
+         "txt_returning_token", [%i"Enter your token"];
+         "txt_returning_token_label", [%i"Token"];
+         "txt_button_connect", [%i"Connect"]
+       ]
