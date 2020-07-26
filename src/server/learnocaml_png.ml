f5b7d7ae5b9004507f6795dac29d0e586936f41e
Etienne MARAIS <etienne@maiste.fr>
2020-07-26 15:59:56 +0200

    Produice ppm string

--- src/server/learnocaml_png.ml
+++ src/server/learnocaml_png.ml
@@ -1,2 +1,27 @@
-let to_png_data _data _w _h =
- "TODO"
+(* This file is part of Learn-OCaml.
+ *
+ * Copyright (C) 2019 OCaml Software Foundation.
+ * Copyright (C) 2016-2018 OCamlPro.
+ *
+ * Learn-OCaml is distributed under the terms of the MIT license. See the
+ * included LICENSE file for details. *)
+
+open Bigarray
+
+let to_png_data data w h =
+  let dim = Array1.dim data in
+  let header = [
+    "\n" ; "255" ; "\n" ;
+    string_of_int h ; " " ; string_of_int w ; "\n" ;
+    "P3" ]
+  in
+  let rec loop k acc =
+    if k < dim then
+      let r = data.{k} |> string_of_int in
+      let g = data.{k+1} |> string_of_int in
+      let b = data.{k+2} |> string_of_int in
+      let px = r ^ " " ^ g ^ " " ^ b ^ " " in
+      let acc = px :: acc in
+      loop (k+3) acc
+    else List.rev acc |> String.concat ""
+  in loop 0 header
