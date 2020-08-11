13d4766126bfaf050e2fdeba3a3a7b1daee69fbc
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-08-12 00:59:11 +0200

    fix: macOS build
    * Add "-j 2" option as well.

--- .ci-macosx.sh
+++ .ci-macosx.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 brew update
+brew install pkg-config
 brew install opam
 brew install libev
 opam init -y --compiler=4.05.0
 eval $(opam env)
 
-opam install -y . --deps-only --locked
+opam install -y -j 2 . --deps-only --locked
 make && make opaminstall
