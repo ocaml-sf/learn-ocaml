8f235f4b8aa12d34e7330db93d86c01c1615d1ef
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-09-13 14:27:33 +0200

    feat(learnocaml_validate_main.ml): Redirect when closing confim dialog

--- src/app/learnocaml_validate_main.ml
+++ src/app/learnocaml_validate_main.ml
@@ -5,9 +5,30 @@
  * Learn-OCaml is distributed under the terms of the MIT license. See the
  * included LICENSE file for details. *)
 
-open Js_utils
 open Learnocaml_common
+
+let rec drop_2_trailing = function
+  | [] | [_] | [_; _] -> []
+  | x :: l -> x :: drop_2_trailing l
+
+(* Replace location: from [http://localhost:8080/confirm/...handle...]
+   to [http://localhost:8080] *)
+let redirect () =
+  let open Js_of_ocaml__Url in
+  match Url.Current.get () with
+  | Some (Http http_url) ->
+     let new_url = {http_url with hu_path = drop_2_trailing http_url.hu_path} in
+     Url.Current.set (Http new_url)
+  | Some (Https http_url) ->
+     let new_url = {http_url with hu_path = drop_2_trailing http_url.hu_path} in
+     Url.Current.set (Https new_url)
+  | Some _ | None -> ()
 
 let () =
   (match Js_utils.get_lang () with Some l -> Ocplib_i18n.set_lang l | None -> ());
-  alert ~title:[%i"EMAIL CONFIRMED"] [%i"Your e-mail address has been confirmed. You can now login."]
+  let message =
+    [%i"Your e-mail address has been confirmed. You can now login."] in
+  let module H =Tyxml_js.Html in
+  ext_alert ~title:[%i"EMAIL CONFIRMED"]
+    ~buttons: [box_button [%i"OK"] @@ redirect]
+    [ H.p [H.txt (String.trim message)] ]
