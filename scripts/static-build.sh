1150bfab233e791cbaf51b0b7ad23714d2a827da
Louis Gesbert <louis.gesbert@ocamlpro.com>
2021-07-16 14:57:42 +0200

    Static builds: use an environment variable rather than files laying around
    Thanks @gasche for the hint about this hidden dune feature :)

--- scripts/static-build.sh
+++ scripts/static-build.sh
@@ -11,8 +11,9 @@ tar c $(git ls-files) | \
   docker run --rm -i \
     ocamlpro/ocaml:4.05 \
     sh -uexc \
-      'tar x >&2 && ./scripts/setup-static-build.sh &&
+      'tar x >&2 &&
+       sudo apk add openssl-libs-static >&2 &&
        opam switch create . ocaml-system "dune<2" --deps-only >&2 &&
-       opam exec make >&2 &&
+       opam exec make LINKING_MODE=static >&2 &&
        tar c -hC _build/install/default/bin .' | \
   tar vx
