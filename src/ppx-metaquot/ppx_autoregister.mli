99e913d847c54e021669c7068ca991ae24de89f2
Louis Gesbert <louis.gesbert@ocamlpro.com>
2022-04-13 10:09:00 +0200

    refactor: Rename and generalise `recorder` to `ppx_autoregister`
    Functorising to add parameters so that it can be used to inject printer
    registerers as well, for example.

---
+++ src/ppx-metaquot/ppx_autoregister.mli
@@ -0,0 +1,8 @@
+module type ARG = sig
+  val val_prefix: string
+  val inject_def: string -> string Ppxlib.loc -> Ppxlib.expression
+end
+
+module Make (_: ARG): sig
+  val expand: Ppxlib.structure -> Ppxlib.structure
+end
