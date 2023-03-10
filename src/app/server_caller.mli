c4efeb33ba3e8b7fc5660cc566ebf7af0cfa6ab5
Yann Regis-Gianas <yann@regis-gianas.org>
2023-03-10 13:33:56 +0100

    docs: Adjust starting copyright year for OCamlPro

--- src/app/server_caller.mli
+++ src/app/server_caller.mli
@@ -1,7 +1,7 @@
 (* This file is part of Learn-OCaml.
  *
  * Copyright (C) 2019 OCaml Software Foundation.
- * Copyright (C) 2016-2018 OCamlPro.
+ * Copyright (C) 2015-2018 OCamlPro.
  *
  * Learn-OCaml is distributed under the terms of the MIT license. See the
  * included LICENSE file for details. *)
