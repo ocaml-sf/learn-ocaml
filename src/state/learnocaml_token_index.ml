86f1bcc9d50cd5ff85c60c6eb0c87ef46b0bc7fb
Louis Tariot <louis.tariot@irit.fr>
2023-07-05 11:26:31 +0200

    feat: implementation of the new cache data structure

--- src/state/learnocaml_token_index.ml
+++ src/state/learnocaml_token_index.ml
@@ -8,8 +8,10 @@
 
 open Lwt
 open Learnocaml_data
+open Lwt.Syntax
 
 let ( / ) dir f = if dir = "" then f else Filename.concat dir f
+let sync_dir = "sync"
 let indexes_subdir = "data"
 
 let logfailwith str arg =
@@ -20,119 +22,44 @@ let generate_random_hex len =
   Cryptokit.Random.string Cryptokit.Random.secure_rng len
   |> Cryptokit.transform_string @@ Cryptokit.Hexa.encode ()
 
-module J = Json_encoding
+module type IndexKV  = functor (Store: Irmin.S) -> sig
+  type token = Learnocaml_data.Token.t
+  type t
 
-module Json_codec = struct
-  let decode enc s =
-    (match s with
-     | "" -> `O []
-     | s -> Ezjsonm.from_string s)
-    |> J.destruct enc
-
-  let encode ?minify enc x =
-    match J.construct enc x with
-    | `A _ | `O _ as json -> Ezjsonm.to_string ?minify json
-    | `Null -> ""
-    | `Bool v -> string_of_bool v
-    | _ -> assert false
+  val parse : [> `O of (string * [> `String of 'a ]) list ] -> 'a
+  val serialise : 'a -> [> `O of (string * [> `String of 'a ]) list ]
+  val read :
+    Store.path list -> (Store.contents -> 'a) -> string -> 'a list Lwt.t
+  val write :
+    Store.path list ->
+    ('a -> Store.contents) -> 'a list -> string -> unit Lwt.t
+  val create_index : string -> unit Lwt.t
+  val exists : t
+  val remove : t
 end
 
-module Store = Irmin_git_unix.FS.KV (Irmin.Contents.String)
+module AUTH: IndexKV = struct
 
-module type IndexRW = sig
-  val config : Irmin.config
-  val read : string -> (string -> 'a) -> 'a Lwt.t
-  val write : string -> ('a -> string) -> 'a -> unit Lwt.t
-end
+  module Store = Irmin_mem.KV.Make(Irmin.Contents.Json_value)
+  module Info = Irmin_unix.Info(Store.Info)
 
-module IndexFile: IndexRW = struct
-  let read keys parse =
+  let read keys parse path=
+    let config = Irmin_git.config ~bare:true path in
     let* repo = Store.Repo.v config in
     let* t = Store.main repo in
     Lwt_list.map_p
-      (fun key -> 
-        parse @@ Store.get t key)
+      (fun key ->
+        let+ x = Store.get t key in parse x)
       keys
 
-  let write keys serialise data_list =
+  let write keys serialise data_list path=
+    let config = Irmin_git.config ~bare:true path in
     let* repo = Store.Repo.v config in
     let* t = Store.main repo in
     Lwt_list.iter_p
       (fun (key,data) ->
-        Store.set_exn t ~info:(Irmin_git_unix.info ~author:"author" "message") key
+        Store.set_exn t ~info:(Info.v "message") key
+          (*deal with the errors if using `set` instead of `set_exn`*)
           (serialise data))
-      List.combine keys data_list
+    @@ List.combine keys data_list
 end
-
-module BaseTokenIndex (RW: IndexRW) = struct
-  let path = (sync_dir / indexes_subdir / "token")
-  let config = Irmin_git.config ~bare:true path
-  
-  let file = "token.irmin"
-  
-  let parse token = token
-  
-  let serialise_str string = string
-  let serialise = String.concat "-"
-
-  let create_index sync_dir =
-    let found_indexes =
-      let rec scan f d acc =
-        let rec aux s acc =
-          Lwt.catch (fun () ->
-              Lwt_stream.get s >>= function
-              | Some ("." | ".." | "data") -> aux s acc
-              | Some x -> scan f (d / x) acc >>= aux s
-              | None -> Lwt.return acc)
-          @@ function
-          | Unix.Unix_error (Unix.ENOTDIR, _, _) -> f d acc
-          | Unix.Unix_error _ -> Lwt.return acc
-          | e -> Lwt.fail e
-        in
-        aux (Lwt_unix.files_of_directory (sync_dir / d)) acc
-      in
-      scan (fun d acc ->
-          let d =
-            if Filename.basename d = "save.json" then Filename.dirname d
-            else d
-          in
-          let stok = String.map (function '/' | '\\' -> '-' | c -> c) d in
-          if Token.check stok then
-            Lwt.return (stok :: acc)
-          else
-            Lwt.return acc
-        ) "" [] in
-    Lwt_io.printl "[INFO] Regenerating the token index..." >>= fun () ->
-    found_indexes >>= RW.write found_indexes serialise_str found_indexes
-
-  let get_file sync_dir name =
-    let tree = Store.get_tree t path in
-    let entries = Store.Tree.list tree [] in
-    let keys = List.map (fun (key,_) -> key) entries in
-    let create () =
-          create_index sync_dir >>= fun () ->
-          RW.read keys parse in
-    if Sys.file_exists path  then
-      Lwt.catch
-        (fun () -> RW.read keys parse)
-        (fun _exn ->
-           (* Note: this error handler may be adapted later to be more conservative?
-              it does not matter now as sync/data/token.json is not a critical file, and
-              can be regenerated. *)
-           create ())
-    else
-      create ()
-
-  let get_tokens sync_dir =
-    get_file sync_dir file
-
-  let add_token sync_dir token =
-    get_tokens sync_dir >>= fun tokens ->
-    if not (List.exists (fun found_token -> found_token = token) tokens) then
-      RW.write path serialise (token :: tokens)
-    else
-      Lwt.return_unit
-end
-
-module TokenIndex = BaseTokenIndex (IndexFile)
-
